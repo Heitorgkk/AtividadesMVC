ea00a8a7a17865ffe0feae3c5cf2ecab29e9ed14
HGK <heitor_klimkowski@estudante.sesisenai.org.br>
2026-04-24 21:42:35 -0300

    initial commit

---
+++ Conversor de Unidades de Producao/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import model.entity.Motor;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ Conversor de Unidades de Producao/src/main/java/view/MenuConverter.java
@@ -0,0 +1,15 @@
+package view;
+
+import java.util.Scanner;
+
+public class MenuConverter {
+
+    public static void menu(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Conversão de ");
+        System.out.printf("Insira o valor: ");
+
+    }
+
+}
